5b74a441f2413ae84dea1ba26c8a3ddb6ffe7ed0
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-05-10 16:37:54 +0530

    Add initial project files and .gitignore for C# HelloWorld example

---
+++ C#-Basics/01.HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Pragram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+        }
+    }
+}
+
