a2298abca4c5b52fca99c1b2e998bc391c6ee90f
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-05-11 12:37:29 +0530

    Add project files for Variables example in C# Basics

---
+++ C#-Basics/02.Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Variables
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int num = 10;
+            Console.WriteLine(num);
+
+            int a, b, c;
+            a = b = c = 6;
+            Console.WriteLine($"{a} {b} {c}");
+
+            Boolean isFlag = true;
+            Console.WriteLine(isFlag);
+        }
+    }
+}
