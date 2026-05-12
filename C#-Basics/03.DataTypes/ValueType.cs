a0ae5e1877fcae20d0cc7175f623f4a95299f91b
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-05-12 19:40:59 +0530

    Add ValueDataType class with ValueIntegerTypeDemo method for demonstrating C# value types

---
+++ C#-Basics/03.DataTypes/ValueType.cs
@@ -0,0 +1,43 @@
+using static System.Console;
+
+namespace DataTypes
+{
+    class ValueDataType
+    {
+        public static void ValueIntegerTypeDemo()
+        {
+            //1. byte (8-bit Unsigned)
+            byte redColorValue = 255;
+            WriteLine($"byte: {redColorValue}");
+
+            //2. sbyte (8-bit Signed)
+            sbyte winterTemperature = -15;
+            WriteLine($"sbyte: {winterTemperature}");
+
+            //3. short (16-bit Signed)
+            short playerAmmo = 12000;
+            WriteLine($"short: {playerAmmo}");
+
+            //4. ushort (16-bit Unsigned)
+            ushort serverPort = 8080;
+            WriteLine($"ushort: {serverPort}");
+
+            //5. int (32-bit Signed)
+            int websiteViews = -1500000;
+            WriteLine($"int: {websiteViews}");
+
+            //6. uint (32-bit Unsigned)
+            uint totalStars = 3500000000;
+            WriteLine($"uint: {totalStars}");
+
+            //7. long (64-bit Signed)
+            long databaseRowId = 854775807123;
+            WriteLine($"long: {databaseRowId}");
+
+            //8. ulong (64-bit Unsigned)
+            ulong hashKey = 18446744073709551615ul;
+            WriteLine($"ulong: {hashKey}");
+
+        }
+    }
+}
