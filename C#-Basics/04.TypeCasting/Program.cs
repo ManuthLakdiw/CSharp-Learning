44d4be5c98aa41c72d87a5bf065473ab5baff27b
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-05-13 13:19:23 +0530

    Refactor comments in Program.cs to clarify manual type casting using .net methods examples

--- C#-Basics/04.TypeCasting/Program.cs
+++ C#-Basics/04.TypeCasting/Program.cs
@@ -19,12 +19,20 @@ namespace TypeCasting
             // Console.WriteLine(myDouble);   // Outputs 9
 
 
-            double myDouble = 9.78;
-            // We use (int) to FORCE the computer to do it, even though we lose the .78
-            int myInt = (int)myDouble; // Manual casting: double to int
+            // double myDouble = 9.78;
+            // // We use (int) to FORCE the computer to do it, even though we lose the .78
+            // int myInt = (int)myDouble; // Manual casting: double to int
 
-            Console.WriteLine(myDouble); // Outputs 9.78
-            Console.WriteLine(myInt);    // Outputs 9
+            // Console.WriteLine(myDouble); // Outputs 9.78
+            // Console.WriteLine(myInt);    // Outputs 9
+
+
+            // Convert Class
+            string ageText = "25";
+            // We translate the text into a real math number!
+            int realAge = Convert.ToInt32(ageText);
+            WriteLine(realAge);
+            WriteLine(realAge.GetType());
         }
     }
 }
