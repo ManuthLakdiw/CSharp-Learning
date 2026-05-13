e6f41f608267d3ebe0e4f2eebcc970f5a61d8e5f
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-05-13 13:07:59 +0530

    Add TypeCasting project and initial Program class to demonstrate type casting in C#

---
+++ C#-Basics/04.TypeCasting/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace TypeCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z;
+            z = "Hiii";
+
+            WriteLine(z); // error CS0029: Cannot implicitly convert type 'string' to 'int'
+        }
+    }
+}
